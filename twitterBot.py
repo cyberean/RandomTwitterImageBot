a338c053ab10ecc6af5e9274864193d805fa0588
cyberangel <103903964+unfaded@users.noreply.github.com>
2024-02-20 22:55:52 -0600

    TwitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -7,17 +7,19 @@ import time
 from configs import client,api
 from datetime import datetime, timedelta
 
-api_key = "3646rx2Lquwl38thRP8GtYAJi"
-api_secret = "SUnDjChTNiQCaRW2gqdiht5qyZErB9sUi1O3msGoRBxpdLW4YC"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAKXYsAEAAAAAvbb5KtudGnK79MPJEoQs9P7vf7Y%3DVWRJhLUNBBddJ2OU5yy3bhXFLviOQYA4OTLecU87bxNWr5J5tD"
-access_token = "1753627689389330432-Gucbeu78AzluplU0RA5MQEvaLo0RhP"
-access_token_secret = "H8PB049lhogVadWVsBSGl9D1IcDbL9zS3GfkhGEBiFf8C"
+api_key = "INPUT_HERE"
+api_secret = "INPUT_HERE"
+bearer_token = "INPUT_HERE"
+access_token = "INPUT_HERE"
+access_token_secret = "INPUT_HERE"
 
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
+
+# Chooses a random image from your folder path
 def tweet_action():
-    folder_path = "C:\\Users\\abdul\\OneDrive\\Desktop\\onepiece"
+    folder_path = "YOUR_FOLDER_PATH_HERE"
     contents = os.listdir(folder_path)
     image_files = [
         file
@@ -32,8 +34,9 @@ def tweet_action():
     image_io = io.BytesIO(image_content)
     media = api.media_upload(filename=random_image, file=image_io)
     client.create_tweet(text="#OnePiece", media_ids=[media.media_id])
+# You don't need to have text of course, I created a One Piece themed twitter bot and used the One Piece hashtag for more visibility
 
-
+# Change the value to however long you want it, I set it to 15 seconds to see if it worked.
 schedule.every(15).seconds.do(tweet_action)
 
 while True:
